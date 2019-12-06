b91961d3c4cd2437a1220fda8c52ae4998e99e3a
colorfuljor <913864870@qq.com>
2019-12-06 12:33:29 +0800

    add store.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
